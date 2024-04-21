c77d7e8266c56d26c4f163aaa6df44452f4482e2
Marcin <marcin.konow@lczyk.xyz>
2024-04-22 00:25:26 +0100

    --dont-test-inplace

--- makefile
+++ makefile
@@ -23,7 +23,7 @@ install: build_wheel
 	pip install --force-reinstall dist/*.whl
 
 clean:
-	rm -rf .mypy_cache .pytest_cache .tox/
+	rm -rf .mypy_cache .pytest_cache
 	rm -rf _skbuild
 	rm -rf build dist MANIFEST.in
 	rm -rf fibbymodule.c ffib1-f2pywrappers.f
@@ -31,5 +31,8 @@ clean:
 	find . -name '*.egg-info' -type d | xargs rm -rf  
 	find . -name '__pycache__' -type d | xargs rm -rf  
 
-test:
-	python -m unittest discover -v -s tests
+test: build_inplace
+	pytest -v
+
+tox: clean
+	tox
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,20 @@
-import os, sys
+from pathlib import Path
+import sys
 
-__project_root__ = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-sys.path.insert(0, __project_root__)
+# Add a command line option to pytest to specify the path to the project root
+def pytest_addoption(parser):
+    parser.addoption(
+        "--dont-test-inplace",
+        action="store_true",
+        default=False,
+        help="Don't test the package in-place",
+    )
+
+
+# Add the project root to the sys.path
+def pytest_configure(config):
+    if config.getoption("--dont-test-inplace"):
+        return
+    project_root = Path(__file__).parent.parent
+    sys.path.insert(0, str(project_root))
